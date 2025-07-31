f58aeffa667d23adfa8b054f06d9b274d43df704
Gabriel Batista Azevedo Furtado <gabriel.furtado@paytrack.com.br>
2025-07-30 21:05:22 -0300

    padrao MVC

--- freteapi/src/main/java/com/gabrielbatista/freteapi/application/FreteapiApplication.java
+++ freteapi/src/main/java/com/gabrielbatista/freteapi/application/FreteapiApplication.java
@@ -1,4 +1,4 @@
-package com.gabrielbatista.freteapi;
+package com.gabrielbatista.freteapi.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
